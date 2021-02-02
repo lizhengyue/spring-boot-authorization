103a4eaa27b4fd68135f9230bc873b4d002ae092
lizhengyue <290558053@qq.com>
2021-02-03 00:17:45 +0800

    增加多租户功能

--- src/main/java/com/demo/config/MybatisPlusConfiguration.java
+++ src/main/java/com/demo/config/MybatisPlusConfiguration.java
@@ -1,35 +1,29 @@
 package com.demo.config;
 
-
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import net.sf.jsqlparser.expression.LongValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import net.sf.jsqlparser.expression.Expression;
-@Configuration //告诉spring这是一个配置类
+@Configuration
 public class MybatisPlusConfiguration {
-
 	@Bean
 	public MybatisPlusInterceptor mybatisPlusInterceptor() {
 		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
 		interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
-
 			@Override
 			public String getTenantIdColumn() {
 				return "manager_id";
 			}
+			//设置租户的值
 			@Override
 			public Expression getTenantId() {
 				return new LongValue(1088248166370832385L);
 			}
-
 			// 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
 			@Override
 			public boolean ignoreTable(String tableName) {
