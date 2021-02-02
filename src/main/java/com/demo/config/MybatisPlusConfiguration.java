b6df76b6ac178c210832593b3771c8553c43d556
李正悦 <290558053@qq.com>
2021-02-02 10:21:07 +0800

    增加自动填充功能

--- src/main/java/com/demo/config/MybatisPlusConfiguration.java
+++ src/main/java/com/demo/config/MybatisPlusConfiguration.java
@@ -1,9 +1,38 @@
 package com.demo.config;
 
-import com.baomidou.mybatisplus.core.injector.ISqlInjector;
-import org.springframework.context.annotation.Configuration;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import net.sf.jsqlparser.expression.LongValue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import net.sf.jsqlparser.expression.Expression;
 @Configuration //告诉spring这是一个配置类
 public class MybatisPlusConfiguration {
 
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
+			@Override
+			public Expression getTenantId() {
+				return new LongValue(1);
+			}
+			// 这是 default 方法,默认返回 false 表示所有表都需要拼多租户条件
+			@Override
+			public boolean ignoreTable(String tableName) {
+				return !"user".equalsIgnoreCase(tableName);
+			}
+		}));
+		// 如果用了分页插件注意先 add TenantLineInnerInterceptor 再 add PaginationInnerInterceptor
+		// 用了分页插件必须设置 MybatisConfiguration#useDeprecatedExecutor = false
+		//interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+		return interceptor;
+	}
 }
